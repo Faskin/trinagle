a7f0037037f6438bd6b635f4cd51e49105bb76b6
Opilane TTHK <opilane@tthk.ee>
2020-10-14 11:01:23 +0300

    Add project files.

---
+++ trinagle/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trinagle
+{
+    public partial class Form1 : Form
+    {
+        private Button Btn1;
+        public Form1()
+        {
+            InitializeComponent();
+            /*Button Btn1 = new Button();
+            {
+                Btn1.Text = "Запуск";
+                Btn1.Location = new Point(544, 96);
+                Btn1.Cursor = Cursors.Hand;
+                Btn1.BackColor = Color.FromArgb(255, 255, 192);
+                Btn1.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+                Btn1.FlatAppearance.BorderSize = 10;
+                Btn1.FlatStyle = FlatStyle.Flat;
+
+            };*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
